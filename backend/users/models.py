24ac51cbc6e6c71fe42fbba745c0674b1089b657
RahulGopathi <gopathi.1@iitj.ac.in>
2022-07-12 18:02:36 +0530

    Add retailer authentication

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from users.managers import CustomUserManager
 
 
 class Owner(models.Model):
@@ -12,11 +12,17 @@ class Owner(models.Model):
         return f"{self.name} {self.wallet_address}"
 
 
-class Retailer(models.Model):
-    name = models.CharField(max_length=100)
-    wallet_address = models.CharField(max_length=250)
-    email = models.EmailField(blank=True)
-    phno = models.CharField(max_length=10, blank=True)
+class Retailer(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def __str__(self):
-        return f"{self.name} -> {self.wallet_address}"
+        return self.email
+
+    class Meta:
+        verbose_name = "Retailer"
