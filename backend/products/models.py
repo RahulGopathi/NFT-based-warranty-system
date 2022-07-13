dfc08df624e90c746c31bd91484e4f0a92fa17b6
RahulGopathi <gopathi.1@iitj.ac.in>
2022-07-13 11:31:50 +0530

    Fix linting errors

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     tokenizable_data = models.FileField(upload_to='products/', null=True, blank=True)  # this would be the image or data that we tokenize
