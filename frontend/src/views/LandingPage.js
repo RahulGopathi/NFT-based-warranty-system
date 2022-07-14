652f93cb2b35651ac427d9515c34135e16f91536
PanyalaSainathReddy <reddy.17@iitj.ac.in>
2022-07-14 23:36:52 +0530

    Resolves Homepage issue

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -4,7 +4,7 @@ import { Button } from '../components/Button';
 import './LandingPage.css';
 // import Navbar from '../components/Navbar';
 import { WalletContext } from '../contexts/WalletContext';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function LandingPage() {
   const navigate = useNavigate();
@@ -21,6 +21,10 @@ function LandingPage() {
     }
   };
 
+  const redirectRetailer = () => {
+    navigate('/retailer-login');
+  };
+
   useEffect(() => {
     videoRef.current.playbackRate = 0.6;
   });
@@ -29,6 +33,7 @@ function LandingPage() {
     <div className="landing-page">
       {/* <Navbar /> */}
       <video src="/landing-video.mp4" ref={videoRef} autoPlay loop muted />
+      <div id="overlay"></div>
       <div className="hero-container">
         <h1>Prove Your</h1>
         <h2>Digital Ownership</h2>
@@ -46,13 +51,9 @@ function LandingPage() {
             className="btns"
             buttonStyle="btn--outline"
             buttonSize="btn--large"
+            onClick={redirectRetailer}
           >
-            <Link
-              style={{ textDecoration: 'none', color: 'white' }}
-              to="/retailer-login"
-            >
-              RETAILER <i className="fa-solid fa-angle-right"></i>
-            </Link>
+            RETAILER <i className="fa-solid fa-angle-right"></i>
           </Button>
         </div>
       </div>
