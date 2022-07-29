69ed8a025007b93c7267cb2a61e66736838b3e92
RahulGopathi <gopathi.1@iitj.ac.in>
2022-07-30 03:15:05 +0530

    implement custmer signup

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,10 +1,5 @@
-from django.conf import settings
-from django.core.files import File
 from rest_framework import serializers
 from .models import Product, Item
-from users.models import Owner
-from .utils import serialize_image
-import os
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -45,21 +40,6 @@ class ItemSerializer(serializers.ModelSerializer):
 
     def get_product(self, obj):
         return ProductSerializer(obj.product, get_items=False).data
-
-    def create(self, validated_data):
-        try:
-            owner_data = validated_data.pop('owner')
-            owner = Owner.objects.create(**owner_data)
-        except KeyError:
-            owner = None
-        product = validated_data.pop('product')
-        image = product.image.path
-        item = Item.objects.create(owner=owner, product=product, **validated_data)
-        ipfs_hash, image_url = serialize_image(image, validated_data['serial_no'])
-        item.image_ipfs = ipfs_hash
-        item.warranty_image = File(open(os.path.join(settings.MEDIA_ROOT, image_url), 'rb'), name=image_url.split('/')[-1])
-        item.save()
-        return item
 
 
 class UpdateItemSerializer(serializers.ModelSerializer):
